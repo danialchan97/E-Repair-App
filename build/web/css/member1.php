97c5c204b6fe7c0842a0ed7053fb8b6bb0502db2
danialchan97 <118067758+danialchan97@users.noreply.github.com>
2022-11-14 13:34:00 +0800

    Add files via upload

---
+++ build/web/css/member1.php
@@ -0,0 +1,44 @@
+<?php
+
+include('header.php')
+
+?>
+<!DOCTYPE HTML>
+ <html> 
+ <head>    
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">     
+
+  <title>Simply</title>     
+  <!--[if lt IE 9]-->   
+
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>     
+
+  <!--[endif]--> 
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/index.css">  
+<link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet' type='text/css'> 
+
+</head>   
+
+<body style="background-image:url('img/13.jpg');">    
+
+
+
+
+
+  <div class="wrapper">
+  <div class = "container" >  
+  	<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+  	<h1>Car Park Booking</h1> 
+  <h3>Availability Parking</h3>
+  </div>
+</div>
+</body>
